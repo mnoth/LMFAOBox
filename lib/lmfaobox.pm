d2e31518f0a6c3e37a9a885ef675b930cfdfa5ae
Will Orr <will@worrbase.com>
2011-04-18 04:59:47 -0400

    Emailing *might* work

--- lib/lmfaobox.pm
+++ lib/lmfaobox.pm
@@ -1,15 +1,20 @@
 package lmfaobox;
 use Dancer ':syntax';
 use Dancer::Plugin::Database;
+use Dancer::Plugin::Email;
 use Dancer::Plugin::FlashMessage;
+
+use Data::Dumper;
+
+use 5.012;
 
 our $VERSION = '1.0';
 
 get '/' => sub {
     if ($ENV{REMOTE_USER}) {
-        template 'admin';
-    } else {
         template 'index';
+    } else {
+        template 'admin';
     }
 };
 
@@ -21,6 +26,19 @@ post '/add/address' => sub {
         or flash(error => $DBI::errstr);
 
     flash(info => 'Added your address');
+    redirect '/';
+};
+
+post '/message' => sub {
+    foreach my $member (database->quick_select('members', { 1 => 1 } )) {
+        email {
+            to => $member->{address},
+            from => 'ay1244@gmail.com',
+            message => params->{'message'},
+        };
+    }
+
+    flash(info => 'Message sent');
     redirect '/';
 };
 
